e020813caa1efe8ac8bd786e8da78264b82c1601
SaladClimbing <84188441+SaladClimbing@users.noreply.github.com>
2024-05-08 21:45:36 -0400

    Can now view entries Its messy, I'm tired

--- code.py
+++ code.py
@@ -1,24 +1,30 @@
 import board
 import digitalio
-from menu import MainMenu
+from menu import MainMenu, ViewEntries, MenuStates
 from encoder import Encoder
 from database import PasswordDatabase
 display = board.DISPLAY
 
+db = PasswordDatabase("/passwords.json")
+keys = list(db.data.keys())
+
 main_menu = MainMenu(display)
+view_entries = ViewEntries(display)
 rotary_encoder = Encoder()
 
 last_position = None
 position = None
-
-db = PasswordDatabase("/passwords.json")
-db.add_password("Google", "user", "password")
+state = MenuStates.MAIN_MENU
 
 # Main loop
 while True:
     position = rotary_encoder.get_position()
     
-    main_menu.update_label(position)
+    if state == MenuStates.MAIN_MENU:
+        main_menu.update_label(position)
+
+    if state == MenuStates.VIEW_ENTRIES:
+        view_entries.update_label(position, keys[0], keys[1], keys[2])
 
     if position != last_position:
         last_position = position
@@ -27,6 +33,14 @@ while True:
     if not rotary_encoder.button.value and not rotary_encoder.encoder_button_held:
         rotary_encoder.encoder_button_held = True
         main_menu.selected_color = 0x00FF00
+        if state == MenuStates.MAIN_MENU:
+            if position == 0:
+                state = MenuStates.VIEW_ENTRIES
+                view_entries.update_all(keys[0], keys[1], keys[2])
+            elif position == 1:
+                state = MenuStates.NEW_ENTRY
+            elif position == 2:
+                state = MenuStates.SETTINGS
         print("Button pressed")
 
     if rotary_encoder.button.value and rotary_encoder.encoder_button_held:
--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ class MainMenu:
 	def __init__(self, display):
 		self.font = bitmap_font.load_font("/fonts/35-Adobe-Helvetica-Bold.bdf")
 		self.color = 0xFFFFFF
-		self.selected_color = 0xFF0000
+		self.selected_color = 0xFFFF00
 
 		# Text Labels
 		self.text_position_X = 10
@@ -59,3 +59,69 @@ class MainMenu:
 		elif label_index == 2:
 			self.update_settings(self.selected_color)
 		self.update_group()
+
+class ViewEntries:
+	def __init__(self, display):
+		self.font = bitmap_font.load_font("/fonts/35-Adobe-Helvetica-Bold.bdf")
+		self.color = 0xFFFFFF
+		self.selected_color = 0xFFFF00
+
+		# Text Labels
+		self.text_position_X = 10
+		self.text_position_Y = 22
+		self.text_height = 35
+
+		self.display = display
+
+		self.update_all()
+	
+	def update_top_label(self, top, color):
+		self.top_label = label.Label(self.font, text=top, color=color)
+		self.top_label.x = self.text_position_X
+		self.top_label.y = self.text_position_Y
+	
+	def update_middle_label(self, middle, color):
+		self.middle_label = label.Label(self.font, text=middle, color=color)
+		self.middle_label.x = self.text_position_X
+		self.middle_label.y = self.text_position_Y + self.text_height
+
+	def update_bottom_label(self, bottom, color):
+		self.bottom_label = label.Label(self.font, text=bottom, color=color)
+		self.bottom_label.x = self.text_position_X
+		self.bottom_label.y = self.text_position_Y + self.text_height * 2
+
+	def update_label(self, label_index, top="", middle="", bottom=""):
+		self.update_all(top=top, middle=middle, bottom=bottom)
+		if label_index == 0:
+			self.update_top_label(top, self.selected_color)
+		elif label_index == 1:
+			self.update_middle_label(middle, self.selected_color)
+		elif label_index == 2:
+			self.update_bottom_label(bottom, self.selected_color)
+		self.update_group()
+
+	def update_all(self, top="", middle="", bottom="", color=0xFFFFFF):
+		self.top_label = label.Label(self.font, text=top, color=color)
+		self.top_label.x = self.text_position_X
+		self.top_label.y = self.text_position_Y
+
+		self.middle_label = label.Label(self.font, text=middle, color=color)
+		self.middle_label.x = self.text_position_X
+		self.middle_label.y = self.text_position_Y + self.text_height
+
+		self.bottom_label = label.Label(self.font, text=bottom, color=color)
+		self.bottom_label.x = self.text_position_X
+		self.bottom_label.y = self.text_position_Y + self.text_height * 2
+
+	def update_group(self):
+		self.text_group = displayio.Group()
+		self.text_group.insert(0, self.top_label)
+		self.text_group.insert(1, self.middle_label)
+		self.text_group.insert(2, self.bottom_label)
+		self.display.root_group = self.text_group
+
+class MenuStates():
+	MAIN_MENU    = 0,
+	VIEW_ENTRIES = 1,
+	NEW_ENTRY    = 2,
+	SETTINGS     = 3
