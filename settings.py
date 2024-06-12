25d258f8fb87099b894ef49135ec77cf056a9022
SaladClimbing <84188441+SaladClimbing@users.noreply.github.com>
2024-06-11 22:56:23 -0400

    Started on settings menu and changeable settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+# TODO Finish settings menu
+
+class Settings():
+    def __init__(self, display):
+        self.display = display
+        
+        self.password_only = false # Doesn't type the username or give inputs
